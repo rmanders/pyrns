ebe55b1eb8b4527cc2591bb02d0272b7d4277f34
Ryan Anderson <andersr9@mail.uc.edu>
2013-05-28 22:55:34 -0400

    Added prime sieve

--- maths.py
+++ maths.py
@@ -1,4 +1,5 @@
 from fractions import gcd
+from math import *
 
 def lcm(a, b):
 	return a*b / gcd(a,b)
@@ -6,3 +7,18 @@ def lcm(a, b):
 def gcd(a,b):
 	"""Returns the greatest common divisor of a and b"""
 	return fractions.gcd(a,b)
+
+def sieveofera(limit):
+        """
+            Implements a naive sieve of Eratosthenes finding primes
+            less than n
+        """
+        a = [True]*limit
+        ret = []
+
+        for i in xrange(2,len(a)):
+            if a[i]:
+                yield i
+                for j in xrange(i*i, limit, i):
+                    a[j] = False
+                
